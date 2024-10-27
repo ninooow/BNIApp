dd915b6a1be931a053684c500669d7afde072d1e
Nino Auliya Nahara <ninoauliyanahara@gmail.com>
2024-10-27 21:33:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bniapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.bniapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.navOptions
+import androidx.navigation.ui.setupWithNavController
+import com.example.bniapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        with(binding){
+            val navController = findNavController(R.id.nav_host_fragment)
+            bottomNavigationView.setupWithNavController(navController)
+        }
+    }
+}
